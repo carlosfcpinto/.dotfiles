5a10a0700ed50d74d23971cc5178e931c0cf2796
Carlos <carlosfranciscopintoscp@gmail.com>
2023-07-09 17:35:24 +0100

    Nvim

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,21 @@
+local map = vim.api.nvim_set_keymap
+local default_opts = {noremap = true, silent = true}
+
+-----------------------------------------------------------
+-- Neovim shortcuts:
+-----------------------------------------------------------
+
+-- spell :)
+map('n', '<leader>s', ":set spell!<CR>", default_opts)
+
+-- don't use arrow keys
+map('', '<up>', '<nop>', {noremap = true})
+map('', '<down>', '<nop>', {noremap = true})
+map('', '<left>', '<nop>', {noremap = true})
+map('', '<right>', '<nop>', {noremap = true})
+
+-- move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h', default_opts)
+map('n', '<C-j>', '<C-w>j', default_opts)
+map('n', '<C-k>', '<C-w>k', default_opts)
+map('n', '<C-l>', '<C-w>l', default_opts)
